15f3e156b40e844faf1791ee0fb5b2bde406b35d
Grahambone37 <electricgrunt37@gmail.com>
2022-11-07 12:09:46 -0800

    added images and items

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//make an image
+function newImage(name, pathName, left, bottom) {
+    name = document.createElement('img')
+    name.src = `assets/${pathName}`
+    name.style.position = 'fixed'
+    name.style.left = `${left}px`
+    name.style.bottom = `${bottom}px`
+    document.body.append(name)
+    return name
+}
+
+//make an item
+function newItem(name, pathName, left, bottom) {
+    let item = newImage(name, pathName, left, bottom)
+    item.addEventListener('dblclick', function() {
+        item.remove()
+    })
+}
+
+//images
+let greenCharacter;
+newImage(greenCharacter, 'green-character.gif', 100, 100)
+
+let pineTree;
+newImage(pineTree, 'pine-tree.png', 450, 200)
+
+let tree;
+newImage(tree, 'tree.png', 200, 300)
+let pillar;
+newImage(pillar, 'pillar.png', 350, 100)
+let crate;
+newImage(crate, 'crate.png', 150, 200)
+let well;
+newImage(well, 'well.png', 500, 425)
+
+//items
+let sword;
+newItem(sword, 'sword.png', 500, 405)
+let shield;
+newItem(shield, 'shield.png', 165, 185)
+let staff;
+newItem(staff, 'staff.png', 600, 100)
+
+
+
