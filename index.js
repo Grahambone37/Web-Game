e6263501e78f0fde674078b9f27ee26a122b49fb
Grahambone37 <electricgrunt37@gmail.com>
2022-11-07 12:43:06 -0800

    added background

--- index.js
+++ index.js
@@ -12,10 +12,30 @@ function newImage(name, pathName, left, bottom) {
 //make an item
 function newItem(name, pathName, left, bottom) {
     let item = newImage(name, pathName, left, bottom)
-    item.addEventListener('dblclick', function() {
+    item.addEventListener('dblclick', function () {
         item.remove()
     })
 }
+
+//make a background
+let lotOfPixels = [0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500]
+function newBackground(name, pathName, repeatUp) {
+    let x = 0
+    let y = 0
+    for (let i = 0; i <= repeatUp; i++) {
+        for (let i = 0; i < lotOfPixels.length; i++) {
+            newImage(name, pathName, x, y)
+            x = lotOfPixels[i]
+        }
+        y = lotOfPixels[i]
+    }
+}
+
+//background
+let sky;
+newBackground(sky, 'sky.png', lotOfPixels.length)
+let grass;
+newBackground(grass, 'grass.png', 5)
 
 //images
 let greenCharacter;
@@ -40,6 +60,7 @@ let shield;
 newItem(shield, 'shield.png', 165, 185)
 let staff;
 newItem(staff, 'staff.png', 600, 100)
+
 
 
 
